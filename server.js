5bc143f32a046ceefaeb56bd94caeff8c0c3a7f2
alvin-odins <alvin.odins@gmail.com>
2022-06-20 12:54:24 +0300

    modified server.js | modified index.html

--- server.js
+++ server.js
@@ -1,49 +1,32 @@
-const express = require('express')
+const express = require( 'express' )
 const app = express()
-const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
+
 
 const PORT = 2022
 require('dotenv').config()
 
-const db_STRING = process.env.DB_STRING
+// db connection
+let db_STRING= process.env.db_STRING
+MongoClient.connect(db_STRING, {useUnifiedTopology:true})
+  .then(client => {
+    console.log(`connected to database`)
+  })
+
+// middleware
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
 
 
-MongoClient.connect(db_STRING, { useUnifiedTopology: true })
-    .then(client => {
-        console.log('Connected to Database')
-        const db = client.db('')
-        
-        const quotesCollection = db.collection('')
-        
+// routes
+app.get( '/', ( request, response ) => {
+  response.render( 'index.ejs' )
+} )
 
 
-        // middleware
-        app.use(bodyParser.urlencoded({ extended: true }))
-        
-        app.set('view engine', 'ejs')
-        
-        app.use(express.static('public'))
-        
-        app.use(bodyParser.json())
-       
-
-
-
-        // routes
-        
-        app.get('/', (request, response) => {
-          response.render('index.ejs')
-          .catch(error => console.log(error))
-        })
-
-
-
-
-
-          app.listen( process.env.PORT || PORT, (  ) => {
-            console.log( `live on port ${PORT}` )
-          } )
-
-    })
-    .catch(error => console.log(error))
+app.listen( process.env.PORT || PORT, () => {
+  console.log(`The server is running on port ${PORT}! You better go catch it`);
+} )
