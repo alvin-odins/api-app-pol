bb5da591c68453a2dd4271596b96242924aade44
alvin-odins <alvin.odins@gmail.com>
2022-06-24 12:59:22 +0300

    modified column in model/leader.js

--- Model/leaders.js
+++ Model/leaders.js
@@ -5,7 +5,7 @@ module.exports = [
     about:
       "Abdelaziz Bouteflika (2 March 1937 – 17 September 2021) was an Algerian politician and diplomat who served as President of Algeria from 1999 to his resignation in 2019. Before his stint as an Algerian politician, Bouteflika served during the Algerian War as a member of the National Liberation Front. After Algeria gained its independence from France, he served as the Minister of Foreign Affairs between 1963 until 1979. He served as President of the United Nations General Assembly during the 1974–1975 session.In 1983 he was convicted of stealing millions of dinars from Algerian embassies during his diplomatic career.",
     tenure: "27 April 1999 – 2 April 2019",
-    "office post": "President",
+    office_post: "President",
     country: "Algeria",
     url: "https://res.cloudinary.com/nkechi-christabel/image/upload/v1656026345/rk4ubaz8uz1u11bbakwc.jpg",
   },
@@ -15,7 +15,7 @@ module.exports = [
     about:
       "Born 5 March 1954. He is an Angolan politician who has served as the president of Angola since 26 September 2017.[3] Previously, he was Minister of Defence from 2014 to 2017. In September 2018 he became the Chairman of the People's Movement for the Liberation of Angola (MPLA), the ruling party. He was the party's Secretary-General from 1998 to 2003.",
     tenure: "26 September 2017 - present",
-    "office post": "President",
+    office_post: "President",
     country: "Angola",
     url: "https://res.cloudinary.com/nkechi-christabel/image/upload/v1656026344/lbvefmayysjknm7ke3yt.jpg",
   },
@@ -26,7 +26,7 @@ module.exports = [
     about:
       "Mokgweetsi Eric Keabetswe Masisi (born 21 July 1962) is the fifth and current President of Botswana, serving since 2018.[4][1][5] He served as the 8th Vice President of Botswana from 12 November 2014 to 1 April 2018. [6]He was a Member of Parliament in the National Assembly for the Moshupa-Manyana constituency from 2009 to 2018.",
     tenure: "1 April 2018 - present",
-    "office post": "President",
+    office_post: "President",
     country: "Botswana",
     url: "https://res.cloudinary.com/nkechi-christabel/image/upload/v1656026344/qtmjjgarfswugcknj4vg.jpg",
   },
@@ -37,7 +37,7 @@ module.exports = [
     about:
       "Roch Marc Christian Kaboré (born 25 April 1957) is a Burkinabé banker and politician who served as the President of Burkina Faso from 2015 until he was deposed in 2022.[1] He was the Prime Minister of Burkina Faso between 1994 and 1996 and President of the National Assembly of Burkina Faso from 2002 to 2012.",
     tenure: "29 December 2015 – 24 January 2022",
-    "office post": "President",
+    office_post: "President",
     country: "Burkina Faso",
     url: "https://res.cloudinary.com/nkechi-christabel/image/upload/v1656026344/ugpxyfw738vtimypfjnl.jpg",
   },
